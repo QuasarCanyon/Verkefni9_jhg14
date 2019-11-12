f1e6bf79873ad12a0d43fe228be18cdea9c086ce
QuasarCanyon <jongudm@gmail.com>
2019-11-12 23:42:23 +0000

    Kominn í gang með scripting

--- scripts.js
+++ scripts.js
@@ -4,8 +4,55 @@ const API_URL = 'https://apis.is/company?name=';
  * Leit að fyrirtækjum á Íslandi gegnum apis.is
  */
 const program = (() => {
-  function init(companies) {
+  let companiesSection;
 
+  function init(companies) {
+    companiesSection = companies;
+  }
+
+  function fetch() {
+    fetch(API_URL + searchString)
+      .then(response => {
+        if (!return.ok) {
+          throw Error("Villa!");
+        }
+        return response.json();
+      })
+      .then(jsonResponse => {
+        show(jsonResponse);
+      })
+      .catch(err => console.error("Upp kom villa!", err));
+  }
+
+  function show() {
+
+  }
+
+  function el(type, className, clickHandler) {
+    let newEl = null;
+    // Todo -- Laga að þeim elementum sem á að búa til hér
+    if (type === 'checkbox') {
+      newEl = document.createElement('input');
+      newEl.setAttribute('type', type);
+    } else {
+      newEl = document.createElement(type);
+    }
+    
+    newEl.setAttribute('class', className);
+
+    if (clickHandler === commit) {
+      newEl.addEventListener('keydown', clickHandler);
+    } else {
+      newEl.addEventListener('click', clickHandler);
+    }
+
+    return newEl;
+  }
+
+  function empty(el) {
+    while (el.firstChild) {
+      el.removeChild(el.firstChild);
+    }
   }
 
   return {
@@ -14,5 +61,6 @@ const program = (() => {
 })();
 
 document.addEventListener('DOMContentLoaded', () => {
+  const companies = document.querySelector('form');
   program.init(companies);
 });
