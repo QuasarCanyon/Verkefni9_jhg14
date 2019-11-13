3fd43b78160537d1b7370924fdcc9adee16b7034
QuasarCanyon <jongudm@gmail.com>
2019-11-13 01:09:28 +0000

    Mikið scriptugums

--- scripts.js
+++ scripts.js
@@ -4,55 +4,52 @@ const API_URL = 'https://apis.is/company?name=';
  * Leit að fyrirtækjum á Íslandi gegnum apis.is
  */
 const program = (() => {
-  let companiesSection;
 
-  function init(companies) {
-    companiesSection = companies;
-  }
-
-  function fetch() {
-    fetch(API_URL + searchString)
-      .then(response => {
-        if (!return.ok) {
-          throw Error("Villa!");
-        }
-        return response.json();
-      })
-      .then(jsonResponse => {
-        show(jsonResponse);
-      })
-      .catch(err => console.error("Upp kom villa!", err));
-  }
-
-  function show() {
-
-  }
-
-  function el(type, className, clickHandler) {
-    let newEl = null;
-    // Todo -- Laga að þeim elementum sem á að búa til hér
-    if (type === 'checkbox') {
-      newEl = document.createElement('input');
-      newEl.setAttribute('type', type);
-    } else {
-      newEl = document.createElement(type);
+  function el(name, ...children) {
+    const element = document.createElement(name);
+    for (let i = 0; i < children.length; i++) {
+      if (typeof children[i] === 'string') {
+        element.appendChild(element.createTextNode(children[i]));
+      } else {
+        element.appendChild(children[i]);
+      }
     }
-    
-    newEl.setAttribute('class', className);
-
-    if (clickHandler === commit) {
-      newEl.addEventListener('keydown', clickHandler);
-    } else {
-      newEl.addEventListener('click', clickHandler);
-    }
-
-    return newEl;
+    return element;
   }
 
   function empty(el) {
     while (el.firstChild) {
       el.removeChild(el.firstChild);
     }
+  }
+
+  function show(data) {
+    if (data.length === 0) {
+      displayError('Ekkert fyrirtæki fannst fyrir leitarstreng.');
+      return;
+    }
+    
+  }
+
+  function fetch(searchString) {
+    debugger;
+    fetch(API_URL + searchString)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error('Villa!');
+        }
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        show(jsonResponse);
+      })
+      .catch((err) => console.error('Upp kom villa!', err));
+  }
+
+  function init(companies) {
+    const companiesSection = companies;
+    const search = companiesSection.target[0].value;
+    companiesSection.addEventListener('submit', fetch(search));
   }
 
   return {
